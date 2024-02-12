d72236bb17c7f6315b3f6f244ad7cb005a3531ff
Emiliano Bayona <emii.bayona22@gmail.com>
2024-02-12 12:42:17 -0300

    Login feat v2

---
+++ components/elements/CustomButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    id="custom-button"
+    :disabled="disabled"
+    class="rounded-l-full rounded-r-full bg-purple hover:bg-purple-dark w-[260px] h-[58px] text-white"
+  >
+    <slot class="text-lg"></slot>
+  </button>
+</template>
+<script setup>
+const props = defineProps({
+  type: { type: String, default: () => "large" },
+  disabled: { type: Boolean, default: false },
+  block: { type: Boolean, default: false },
+});
+
+const customClasses = computed(() => {
+  const res = [];
+  if (props.block) {
+    res.push("w-full");
+  }
+  return res.join(" ");
+});
+</script>
---
+++ components/elements/CustomInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col gap-1">
+    <label v-if="label" class="text-form-label">{{ label }}</label>
+    <input
+      :value="value"
+      :class="style"
+      :placeholder="placeholder"
+      :type="type"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+<script setup>
+import { computed } from "vue";
+defineEmits(["input"]);
+const props = defineProps({
+  value: { type: [String, Number], default: "" },
+  label: { type: [String, Number], default: "" },
+  placeholder: { type: String, default: "" },
+  type: { type: String, default: "text" },
+  error: { type: String, default: null },
+  required: { type: Boolean, default: false },
+});
+const style = computed(() => {
+  const base = [
+    "bg-light-pink h-[56px] text-grey px-3 rounded-lg focus:outline-darker-pink focus:outline-1",
+  ];
+  const extra = [];
+  if (props.error) {
+    extra.push("border-2 border-rose-950");
+  }
+  return [...base, ...extra].join(" ");
+});
+</script>
+
+<style scoped lang="scss">
+//
+</style>
